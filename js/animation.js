8f3ca1df0be98d0fb376da98e887657cc9511b0c
songluk <you@example.com>
2020-02-12 00:46:13 +0700

    Dada commit

---
+++ js/animation.js
@@ -0,0 +1,13 @@
+$(".text-slider").typer({
+    strings: [
+      "Play Store",
+      "Apple Store"
+    ],
+    typeSpeed: 300,
+    backspaceSpeed: 150,
+    backspaceDelay: 1000,
+    repeatDelay: 2000,
+    repeat: true,
+    autoStart: true,
+    startDelay: 500
+  });
---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(function() {
+    $(".toggle").on("click", function() {
+        if ($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+            $(this).find("a").html("<i class='fas fa-bars'></i>");
+        } else {
+            $(".item").addClass("active");
+            $(this).find("a").html("<i class='fas fa-times'></i>");
+        }
+    });
+    
+    $('.post-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
+
+
+});
